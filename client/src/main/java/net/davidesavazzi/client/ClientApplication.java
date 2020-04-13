44de144765649a35690462d58a3dfda26047df53
Davide Savazzi <davide.savazzi@gmail.com>
2020-04-13 11:40:19 +0100

    discovery-server

---
+++ client/src/main/java/net/davidesavazzi/client/ClientApplication.java
@@ -0,0 +1,56 @@
+package net.davidesavazzi.client;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ClientApplication {
+
+	@Autowired
+	EurekaClient eurekaClient;
+
+	@Autowired
+	DiscoveryClient discoveryClient;
+
+	@Autowired
+	RestTemplateBuilder restTemplateBuilder;
+
+	@PostConstruct
+	private void postConstruct() {
+		InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("service", false);
+		System.out.println("Found service: " + instanceInfo.getHomePageUrl());
+
+		ServiceInstance serviceInstance = discoveryClient.getInstances("service").get(0);
+		System.out.println("Found service: " + serviceInstance.getHost() + ":" + serviceInstance.getPort());
+	}
+
+	@RequestMapping("/")
+	public String message() {
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("service", false);
+		ResponseEntity<String> response = restTemplate.exchange(instanceInfo.getHomePageUrl(),
+				HttpMethod.GET, null, String.class);
+		return response.getBody();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
---
+++ client/src/test/java/net/davidesavazzi/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package net.davidesavazzi.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
