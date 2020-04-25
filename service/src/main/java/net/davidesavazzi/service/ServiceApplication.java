a9f22a34214f555ca74ff34aad386399f4d16bba
Davide Savazzi <davide.savazzi@gmail.com>
2020-04-25 15:57:26 +0100

    config-client

--- service/src/main/java/net/davidesavazzi/service/ServiceApplication.java
+++ service/src/main/java/net/davidesavazzi/service/ServiceApplication.java
@@ -4,13 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
 @RestController
 public class ServiceApplication {
+
+	@Value("${some.property}")
+	private String someProperty;
 
 	@Value("${service.instance.name}")
 	private String instance;
@@ -21,6 +26,6 @@ public class ServiceApplication {
 
 	@RequestMapping("/")
 	public String message() {
-		return "Hello from " + instance;
+		return "Hello from " + instance + " (someProperty=" + someProperty + ")";
 	}
 }
