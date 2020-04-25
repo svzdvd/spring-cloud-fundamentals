ad3accf382b921255f407c1a308ce2ad408c2c9b
Davide Savazzi <davide.savazzi@gmail.com>
2020-04-25 10:32:17 +0100

    config-server

--- client/src/main/java/net/davidesavazzi/client/ClientApplication.java
+++ client/src/main/java/net/davidesavazzi/client/ClientApplication.java
@@ -52,5 +52,4 @@ public class ClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
-
 }
---
+++ config-server/README.md
@@ -0,0 +1,5 @@
+Test:
+
+* `GET http://localhost:8888/config-client-app.yml`
+
+* `GET http://localhost:8888/config-client-app.properties`
